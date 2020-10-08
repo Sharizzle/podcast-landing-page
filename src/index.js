d413ee167c567d85e07536006dbc6ab08c7f3a05
Sharizzle <sharif@thekanaan.com>
2020-10-09 00:04:08 +0300

    final non compressed version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
 const toggleNavbar = (collapseID) => {
 	document.getElementById(collapseID).classList.toggle('hidden');
 	document.getElementById(collapseID).classList.toggle('block');
